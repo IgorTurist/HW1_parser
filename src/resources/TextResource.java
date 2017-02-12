802d20486d7fbab4fd72504727b01e45b298ea96
Igor Mikheychev <igor_m_87@mail.ru>
2017-02-12 21:51:38 +0300

    UrlResource tests added.

--- src/resources/TextResource.java
+++ src/resources/TextResource.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 public abstract class TextResource implements Runnable{
 
     static {
-        dict = new HashMap<String,Long>(TextResource.INIT_CAPACITY);
+        dict = new HashMap<>(TextResource.INIT_CAPACITY);
     }
 
     /**
@@ -24,7 +24,7 @@ public abstract class TextResource implements Runnable{
             finish = true;
             log.severe(ex.getMessage());
         }
-        log.info("Thread (id=" + Thread.currentThread().getId() + ") has just been finished.");
+        log.info("Thread (id=" + Thread.currentThread().getId() + ") is going to be finished till since the moment.");
     }
 
     /**
@@ -89,7 +89,7 @@ public abstract class TextResource implements Runnable{
 
     protected static volatile boolean finish = false;
     protected String path = "";
-    protected static final int INIT_CAPACITY = 100;
-    protected static volatile HashMap<String,Long> dict;
+    private static final int INIT_CAPACITY = 100;
+    private static volatile HashMap<String,Long> dict;
     protected static final Logger log = Logger.getLogger(TextResource.class.getName());
 }
